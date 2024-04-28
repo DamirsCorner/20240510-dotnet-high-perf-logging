2da0133add76d2f924c57775e11dae2c89819e73
Damir Arh <damir.arh@gmail.com>
2024-04-28 09:50:59 +0200

    Add project files.

---
+++ HighPerformanceLogging/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Logging;
+
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddConsole();
+});
+
+var logger = loggerFactory.CreateLogger<Program>();
+logger.LogInformation("Hello, world from {Class}!", nameof(Program));
